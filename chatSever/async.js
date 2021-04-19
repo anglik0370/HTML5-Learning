76afd05e29c0307d93e0ec1769a183db269491a9
Anglik <seonwoo6429@gmail.com>
2021-04-19 17:57:47 +0900

    no message

--- chatSever/async.js
+++
@@ -1,9 +0,0 @@
-let a = 10;
-let b = 0;
-
-setTimeout(()=>
-{
-    b = 20;
-}, 1000);
-
-console.log(a + b);
--- chatSever/server.js
+++ chatSever/server.js
@@ -2,27 +2,26 @@ const http = require('http');
 const socketio = require('socket.io');
 const express = require('express');
 const app = new express();
-const server = http.createServer(app);
+const server = http.createServer(app); //미안 서버야...사과할께..
 const path = require('path');
 
-app.get('/', (req, res) =>
-{
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, "views", "index.html"));
 });
-
-const io = socketio(server);
+const io = socketio(server); //이렇게하면 서버에 소켓이 붙는다.
 //io는 서버의 모든 소켓을 관리하는 객체
-
-io.on("connection", socket =>
-{
-    socket.on("chat", data =>
-    {
-        io.emit("idk", {sender:socket.id, msg:data.msg});
+//on은 이벤트를 연결해주는 것으로 addEventListener과 동일
+io.on("connection", socket => {
+    socket.on("chat", data => {
+        // data => {msg:"asdasd", nickName:"오"}
+        let {msg, nickName} = data;
+        io.emit("chat", {sender:socket.id, msg, nickName});
     });
-});
-//on은 이벤트를 연결해주는 것으로 addEventListener와 동일
 
-server.listen(15451, () =>
-{
-    console.log("서버가 15451포트에서 돌아가고 있습니다.");
+});
+
+server.listen(15454, ()=>{
+    console.log("서버가 15454포트에서 돌아가고 있습니다.");
 });
