81cbf993422f2156ec802b7c72dcb9c82a8648d3
Anglik <seonwoo6429@gmail.com>
2021-04-27 20:24:25 +0900

    닷지게임 + 서버 + DB 연동

--- chatSever/DBTest.js
+++ chatSever/DBTest.js
@@ -17,7 +17,38 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "db.html"));
 });
 
+app.get("/game", (req, res)=> {
+    res.sendFile(path.join(__dirname, "views", "dodge.html"));
+});
+
 io.on("connection", socket =>{
+    console.log(`유저 연결 : ${socket.id}`);
+
+    socket.on("savescore", async data => {
+        let {name, score, msg} = data;
+        let con = await connPool.getConnection();
+
+        let sql = `INSERT INTO score_list(name, score, msg) VALUES(?, ?, ?)`;
+
+        let result = await con.query(sql, [name, score, msg]);
+        if(result[0].affectedRows == 1)
+        {
+            socket.emit("msg", {msg:"기록되었습니다."});
+        }
+
+        sql = `SELECT * FROM score_list ORDER BY score DESC LIMIT 0, 5`
+        result = await con.query(sql);
+        socket.emit("refreshscore", result[0]);
+    });
+
+    socket.on("refreshscore", async data => {
+        let con = await connPool.getConnection();
+        const sql = `SELECT * FROM score_list ORDER BY score DESC LIMIT 0, 5`
+        let result = await con.query(sql);
+
+        socket.emit("refreshscore", result[0]);
+    })
+
     socket.on("register", async data => {
         //register라는 메시지로 data가 넘어온다.
         let {name, email, pass} = data;
